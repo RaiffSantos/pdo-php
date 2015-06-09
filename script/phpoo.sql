879ffbe7b6c3efd13d675f39345ac3211f0ae118
raiffsantos <raiff_santos@hotmail.com>
2015-06-08 23:39:18 -0300

    db teste

--- script/phpoo.sql
+++ script/phpoo.sql
@@ -6,7 +6,19 @@ USE phpoo;
 
 -- criando tabela cliente
 CREATE TABLE cliente(
-cliente_id             INT             NOT NULL      PRIMARY KEY        AUTO_INCREMENT,
 cliente_nome           VARCHAR(80)     NOT NULL,
-cliente_email          VARCHAR(140)    NOT NULL
+cliente_email          VARCHAR(140)    NOT NULL,
+cpf                    VARCHAR(11)     NOT NULL
 )Engine = InnoDB; 
+
+-- criando tabela produto
+CREATE TABLE produto(
+nome                   VARCHAR(80)     NOT NULL,
+preco                  FLOAT           NOT NULL,
+quantidade             INT             NOT NULL,
+referencia             INT             NOT NULL
+);
+
+-- inserindo chave primaria
+ALTER TABLE cliente ADD COLUMN cliente_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT FIRST;
+ALTER TABLE produto ADD COLUMN produto_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT FIRST;
